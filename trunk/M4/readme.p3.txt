e058c9504a051866899fcdc053c35cdc20675e42
Justin.Jackson.III@gmail.com <Justin.Jackson.III@gmail.com@c0925c9d-463d-b94c-0a2d-4f8c3f6d2618>
2012-10-02 00:53:06 +0000

    I completed my readme and personal java file.

---
+++ trunk/M4/readme.p3.txt
@@ -0,0 +1,4 @@
+Chris Reynolds
+creynolds7
+902610280
+The mirror broke my face for 7 years good luck.
--- trunk/M4/src/edu/gatech/oad/antlab/person/Person3.java
+++ trunk/M4/src/edu/gatech/oad/antlab/person/Person3.java
@@ -42,7 +42,10 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+		 String temp = "";
+		 for (int i = input.length()-1; i>=0 ; i--) {
+			 temp += input.charAt(i);
+		 }
+		 return temp;
 	}
 }
