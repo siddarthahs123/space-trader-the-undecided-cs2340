7c81fe9bbd155818e05c37cef3ed522891cc34af
Justin.Jackson.III <Justin.Jackson.III@gmail.com>
2012-10-19 02:43:26 +0000

    Created hierarchy in views.

--- M6/src/views/MainView.java
+++ M6/src/views/MainView.java
@@ -1,5 +1,19 @@
 package views;
 
-public class MainView {
+import java.awt.*;
 
+import javax.swing.*;
+
+public class MainView {
+	
+	JFrame frame;
+	
+	public MainView() {
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setPreferredSize(new Dimension(1920,1080));
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
 }
