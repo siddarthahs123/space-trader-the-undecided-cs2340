599fb2becbc4ac7bc761164da2e5f4ecc2eee25b
baohle11@gmail.com <baohle11@gmail.com@c0925c9d-463d-b94c-0a2d-4f8c3f6d2618>
2012-10-22 02:54:43 +0000

    Added UniverseView and UniverseController

---
+++ trunk/M6/src/controllers/UniverseController.java
@@ -0,0 +1,12 @@
+package controllers;
+//Sol
+//Kepler Verge
+//Armstrong Nebula
+//Exodus Cluster
+//Hades' Gamma
+//Bjorno Nebula
+//Serpent Nebula
+//Nubian Expanse
+public class UniverseController {
+
+}
