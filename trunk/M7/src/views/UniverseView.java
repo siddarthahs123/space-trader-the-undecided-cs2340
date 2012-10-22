3a1716baf420058b0bd4f282cfeca6773b780159
Justin.Jackson.III@gmail.com <Justin.Jackson.III@gmail.com@c0925c9d-463d-b94c-0a2d-4f8c3f6d2618>
2012-10-22 23:13:04 +0000

    Synced player info from controller after creation.

--- trunk/M7/src/views/UniverseView.java
+++ trunk/M7/src/views/UniverseView.java
@@ -3,16 +3,35 @@ package views;
 import java.awt.*;
 import javax.swing.*;
 
-public class UniverseView extends MainView {
+import models.*;
+
+public class UniverseView extends JPanel { //maybe implement main view
 	
 	private JPanel panel;
+	private SolarSystem[] galaxies;
 	
 	public UniverseView() {
 		panel = new JPanel();
 		panel.setBounds(6, 6, 679, 473);
 		panel.setLayout(null);
+		panel.setBackground(Color.black);
 		
+	}
+	
+	public void drawGalaxies(SolarSystem[] galaxies) {
+		this.galaxies = galaxies;
+		System.out.println("drawing");
+		repaint();
+	}
+	
+	public void paintComponent(Graphics g) {
+		System.out.println("painting!");
+		g.setColor(Color.blue);
 		
+		for(int i = 0; i < galaxies.length; i++) {
+			g.drawOval(galaxies[i].getX(), galaxies[i].getY(), 10, 10);
+			System.out.println("drawing planet!");
+		}
 	}
 	
 	public JPanel getPanel() {
