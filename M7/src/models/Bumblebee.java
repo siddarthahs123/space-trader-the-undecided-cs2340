f453cdc205be72f3b654063d61638ef60bf9e8f0
baohle11 <baohle11@gmail.com>
2012-10-24 02:46:21 +0000

    Created spaceships

---
+++ M7/src/models/Bumblebee.java
@@ -0,0 +1,12 @@
+package models;
+/**
+ * This class represents the Bumblebee spaceship
+ * @author Bao
+ *
+ */
+public class Bumblebee extends Spaceship{
+	
+	public Bumblebee() {
+		super("Bumblebee", 25, 1, 2, 2, 2, 15, 5, 7, 60000, 125, 15, 100, 1, 2);
+	}
+}
---
+++ M7/src/models/Flea.java
@@ -0,0 +1,12 @@
+package models;
+/**
+ * This class represents the Flea spaceship
+ * @author Bao
+ *
+ */
+public class Flea extends Spaceship{
+
+	public Flea() {
+		super("Flea", 10, 0, 0, 0, 1, 100, 4, 1, 2000, 5, 2, 25, 1, 0);
+	}
+}
