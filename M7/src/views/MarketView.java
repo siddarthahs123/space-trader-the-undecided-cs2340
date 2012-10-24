e9654c97689c38376064692ff54a24ba36e73123
Justin.Jackson.III <Justin.Jackson.III@gmail.com>
2012-10-24 00:29:19 +0000

    Working on getting market place working: stuck on how to distribute goods to planet.

--- M7/src/views/MarketView.java
+++ M7/src/views/MarketView.java
@@ -1,5 +1,30 @@
 package views;
 
-public class MarketView {
+import javax.swing.*;
 
+import models.*;
+
+public class MarketView extends MainView {
+	
+	private JPanel panel;
+	private JLabel planetName;
+	
+	public MarketView() {
+		panel = new JPanel();
+		panel.setBounds(6, 6, 679, 473);
+		panel.setLayout(null);
+		
+		planetName = new JLabel("Planet");
+		planetName.setHorizontalAlignment(SwingConstants.CENTER);
+		planetName.setBounds((panel.getWidth()/2)-87, 20, 174, 16);
+		panel.add(planetName);
+	}
+	
+	public JPanel getPanel() {
+		return panel;
+	}
+	
+	public void setPlanetName(String name) {
+		planetName.setText(name);
+	}
 }
