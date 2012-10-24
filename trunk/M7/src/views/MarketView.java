2fdb2eca89e671c9a2d1bc73a42eb45d2aeb56df
Justin.Jackson.III@gmail.com <Justin.Jackson.III@gmail.com@c0925c9d-463d-b94c-0a2d-4f8c3f6d2618>
2012-10-24 03:11:55 +0000

    Got market partly working.

--- trunk/M7/src/views/MarketView.java
+++ trunk/M7/src/views/MarketView.java
@@ -3,14 +3,23 @@ package views;
 import javax.swing.*;
 
 import models.*;
+import systems.MainController.BuyListener;
+import systems.MainController.SellListener;
 import java.awt.Color;
+import java.awt.event.*;
 
 public class MarketView extends MainView {
 	
 	private JPanel panel;
-	private JLabel planetName;
-	
-	public MarketView() {
+	private JLabel planetName, lblMrobots, lblMore, lblMnarcotics, lblMmedicine, lblMmachines, lblMfirearms, lblMfood,
+		lblMgames, lblMfurs, lblMwater, lblCrobots, lblCore, lblCnarcotics, lblCmedicine, lblCmachines, lblCfirearms,
+		lblCfood, lblCgames, lblCfurs, lblCwater;
+	private ButtonGroup buyGroup, sellGroup;
+	private JButton waterBuy, fursBuy, gamesBuy, foodBuy, firearmsBuy, machinesBuy, medicineBuy, narcoticsBuy, oreBuy,
+		robotsBuy, waterSell, fursSell, gamesSell, foodSell, firearmsSell, machinesSell, medicineSell, narcoticsSell, oreSell,
+		robotsSell;
+
+	public MarketView(BuyListener buyListener, SellListener sellListener) {
 		panel = new JPanel();
 		panel.setBounds(6, 6, 679, 473);
 		panel.setLayout(null);
@@ -80,105 +89,401 @@ public class MarketView extends MainView {
 		lblRobots.setBounds(50, 325, 70, 14);
 		panel.add(lblRobots);
 		
-		JLabel lblCWater = new JLabel("cWater");
-		lblCWater.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCWater.setBounds(175, 100, 61, 14);
-		panel.add(lblCWater);
+		lblCwater = new JLabel("cWater");
+		lblCwater.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCwater.setBounds(175, 100, 61, 14);
+		panel.add(lblCwater);
 		
-		JLabel lblCfurs = new JLabel("cFurs");
+		lblCfurs = new JLabel("cFurs");
 		lblCfurs.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCfurs.setBounds(175, 125, 61, 14);
 		panel.add(lblCfurs);
 		
-		JLabel lblCgames = new JLabel("cGames");
+		lblCgames = new JLabel("cGames");
 		lblCgames.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCgames.setBounds(175, 150, 61, 14);
 		panel.add(lblCgames);
 		
-		JLabel lblCfood = new JLabel("cFood");
+		lblCfood = new JLabel("cFood");
 		lblCfood.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCfood.setBounds(175, 175, 61, 14);
 		panel.add(lblCfood);
 		
-		JLabel lblCfirearms = new JLabel("cFirearms");
+		lblCfirearms = new JLabel("cFirearms");
 		lblCfirearms.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCfirearms.setBounds(175, 200, 61, 14);
 		panel.add(lblCfirearms);
 		
-		JLabel lblCmachines = new JLabel("cMachines");
+		lblCmachines = new JLabel("cMachines");
 		lblCmachines.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCmachines.setBounds(175, 225, 61, 14);
 		panel.add(lblCmachines);
 		
-		JLabel lblCmedicine = new JLabel("cMedicine");
+		lblCmedicine = new JLabel("cMedicine");
 		lblCmedicine.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCmedicine.setBounds(175, 250, 61, 14);
 		panel.add(lblCmedicine);
 		
-		JLabel lblCnarcotics = new JLabel("cNarcotics");
+		lblCnarcotics = new JLabel("cNarcotics");
 		lblCnarcotics.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCnarcotics.setBounds(175, 275, 61, 14);
 		panel.add(lblCnarcotics);
 		
-		JLabel lblCore = new JLabel("cOre");
+		lblCore = new JLabel("cOre");
 		lblCore.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCore.setBounds(175, 300, 61, 14);
 		panel.add(lblCore);
 		
-		JLabel lblCrobots = new JLabel("cRobots");
+		lblCrobots = new JLabel("cRobots");
 		lblCrobots.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCrobots.setBounds(175, 325, 61, 14);
 		panel.add(lblCrobots);
 		
-		JLabel lblMwater = new JLabel("mWater");
+		lblMwater = new JLabel("mWater");
 		lblMwater.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMwater.setBounds(300, 100, 61, 14);
 		panel.add(lblMwater);
 		
-		JLabel lblMfurs = new JLabel("mFurs");
+		lblMfurs = new JLabel("mFurs");
 		lblMfurs.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMfurs.setBounds(300, 125, 61, 14);
 		panel.add(lblMfurs);
 		
-		JLabel lblMgames = new JLabel("mGames");
+		lblMgames = new JLabel("mGames");
 		lblMgames.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMgames.setBounds(300, 150, 61, 14);
 		panel.add(lblMgames);
 		
-		JLabel lblMfood = new JLabel("mFood");
+		lblMfood = new JLabel("mFood");
 		lblMfood.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMfood.setBounds(300, 175, 61, 14);
 		panel.add(lblMfood);
 		
-		JLabel lblMfirearms = new JLabel("mFirearms");
+		lblMfirearms = new JLabel("mFirearms");
 		lblMfirearms.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMfirearms.setBounds(300, 200, 61, 14);
 		panel.add(lblMfirearms);
 		
-		JLabel lblMmachines = new JLabel("mMachines");
+		lblMmachines = new JLabel("mMachines");
 		lblMmachines.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMmachines.setBounds(300, 225, 61, 14);
 		panel.add(lblMmachines);
 		
-		JLabel lblMmedicine = new JLabel("mMedicine");
+		lblMmedicine = new JLabel("mMedicine");
 		lblMmedicine.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMmedicine.setBounds(300, 250, 61, 14);
 		panel.add(lblMmedicine);
 		
-		JLabel lblMnarcotics = new JLabel("mNarcotics");
+		lblMnarcotics = new JLabel("mNarcotics");
 		lblMnarcotics.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMnarcotics.setBounds(300, 275, 61, 14);
 		panel.add(lblMnarcotics);
 		
-		JLabel lblMore = new JLabel("mOre");
+		lblMore = new JLabel("mOre");
 		lblMore.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMore.setBounds(300, 300, 61, 14);
 		panel.add(lblMore);
 		
-		JLabel lblMrobots = new JLabel("mRobots");
+		lblMrobots = new JLabel("mRobots");
 		lblMrobots.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMrobots.setBounds(300, 325, 61, 14);
 		panel.add(lblMrobots);
+		
+		waterBuy = new JButton("Buy");
+		waterBuy.setBounds(403, 94, 61, 29);
+		panel.add(waterBuy);
+		waterBuy.addActionListener(buyListener);
+		
+		fursBuy = new JButton("Buy");
+		fursBuy.setBounds(403, 119, 61, 29);
+		panel.add(fursBuy);
+		fursBuy.addActionListener(buyListener);
+		
+		gamesBuy = new JButton("Buy");
+		gamesBuy.setBounds(403, 144, 61, 29);
+		panel.add(gamesBuy);
+		gamesBuy.addActionListener(buyListener);
+		
+		foodBuy = new JButton("Buy");
+		foodBuy.setBounds(403, 169, 61, 29);
+		panel.add(foodBuy);
+		foodBuy.addActionListener(buyListener);
+		
+		firearmsBuy = new JButton("Buy");
+		firearmsBuy.setBounds(403, 194, 61, 29);
+		panel.add(firearmsBuy);
+		firearmsBuy.addActionListener(buyListener);
+		
+		machinesBuy = new JButton("Buy");
+		machinesBuy.setBounds(403, 219, 61, 29);
+		panel.add(machinesBuy);
+		machinesBuy.addActionListener(buyListener);
+		
+		medicineBuy = new JButton("Buy");
+		medicineBuy.setBounds(403, 244, 61, 29);
+		panel.add(medicineBuy);
+		medicineBuy.addActionListener(buyListener);
+		
+		narcoticsBuy = new JButton("Buy");
+		narcoticsBuy.setBounds(403, 269, 61, 29);
+		panel.add(narcoticsBuy);
+		narcoticsBuy.addActionListener(buyListener);
+		
+		oreBuy = new JButton("Buy");
+		oreBuy.setBounds(403, 294, 61, 29);
+		panel.add(oreBuy);
+		oreBuy.addActionListener(buyListener);
+		
+		robotsBuy = new JButton("Buy");
+		robotsBuy.setBounds(403, 319, 61, 29);
+		panel.add(robotsBuy);
+		robotsBuy.addActionListener(buyListener);
+		
+		waterSell = new JButton("Sell");
+		waterSell.setBounds(476, 94, 61, 29);
+		panel.add(waterSell);
+		waterSell.addActionListener(sellListener);
+		
+		fursSell = new JButton("Sell");
+		fursSell.setBounds(476, 119, 61, 29);
+		panel.add(fursSell);
+		fursSell.addActionListener(sellListener);
+		
+		gamesSell = new JButton("Sell");
+		gamesSell.setBounds(476, 144, 61, 29);
+		panel.add(gamesSell);
+		gamesSell.addActionListener(sellListener);
+		
+		foodSell = new JButton("Sell");
+		foodSell.setBounds(476, 169, 61, 29);
+		panel.add(foodSell);
+		foodSell.addActionListener(sellListener);
+		
+		firearmsSell = new JButton("Sell");
+		firearmsSell.setBounds(476, 194, 61, 29);
+		panel.add(firearmsSell);
+		firearmsSell.addActionListener(sellListener);
+		
+		machinesSell = new JButton("Sell");
+		machinesSell.setBounds(476, 219, 61, 29);
+		panel.add(machinesSell);
+		machinesSell.addActionListener(sellListener);
+		
+		medicineSell = new JButton("Sell");
+		medicineSell.setBounds(476, 244, 61, 29);
+		panel.add(medicineSell);
+		medicineSell.addActionListener(sellListener);
+		
+		narcoticsSell = new JButton("Sell");
+		narcoticsSell.setBounds(476, 269, 61, 29);
+		panel.add(narcoticsSell);
+		narcoticsSell.addActionListener(sellListener);
+		
+		oreSell = new JButton("Sell");
+		oreSell.setBounds(476, 294, 61, 29);
+		panel.add(oreSell);
+		oreSell.addActionListener(sellListener);
+		
+		robotsSell = new JButton("Sell");
+		robotsSell.setBounds(476, 319, 61, 29);
+		panel.add(robotsSell);
+		robotsSell.addActionListener(sellListener);
+		
+		buyGroup = new ButtonGroup();
+		buyGroup.add(waterBuy);
+		buyGroup.add(fursBuy);
+		buyGroup.add(gamesBuy);
+		buyGroup.add(foodBuy);
+		buyGroup.add(firearmsBuy);
+		buyGroup.add(machinesBuy);
+		buyGroup.add(medicineBuy);
+		buyGroup.add(narcoticsBuy);
+		buyGroup.add(oreBuy);
+		buyGroup.add(robotsBuy);
+		
+		sellGroup = new ButtonGroup();
+		sellGroup.add(waterSell);
+		sellGroup.add(fursSell);
+		sellGroup.add(gamesSell);
+		sellGroup.add(foodSell);
+		sellGroup.add(firearmsSell);
+		sellGroup.add(machinesSell);
+		sellGroup.add(medicineSell);
+		sellGroup.add(narcoticsSell);
+		sellGroup.add(oreSell);
+		sellGroup.add(robotsSell);
+	}
+	
+	public JLabel getPlanetName() {
+		return planetName;
+	}
+
+	public JLabel getLblMrobots() {
+		return lblMrobots;
+	}
+
+	public JLabel getLblMore() {
+		return lblMore;
+	}
+
+	public JLabel getLblMnarcotics() {
+		return lblMnarcotics;
+	}
+
+	public JLabel getLblMmedicine() {
+		return lblMmedicine;
+	}
+
+	public JLabel getLblMmachines() {
+		return lblMmachines;
+	}
+
+	public JLabel getLblMfirearms() {
+		return lblMfirearms;
+	}
+
+	public JLabel getLblMfood() {
+		return lblMfood;
+	}
+
+	public JLabel getLblMgames() {
+		return lblMgames;
+	}
+
+	public JLabel getLblMfurs() {
+		return lblMfurs;
+	}
+
+	public JLabel getLblMwater() {
+		return lblMwater;
+	}
+
+	public JLabel getLblCrobots() {
+		return lblCrobots;
+	}
+
+	public JLabel getLblCore() {
+		return lblCore;
+	}
+
+	public JLabel getLblCnarcotics() {
+		return lblCnarcotics;
+	}
+
+	public JLabel getLblCmedicine() {
+		return lblCmedicine;
+	}
+
+	public JLabel getLblCmachines() {
+		return lblCmachines;
+	}
+
+	public JLabel getLblCfirearms() {
+		return lblCfirearms;
+	}
+
+	public JLabel getLblCfood() {
+		return lblCfood;
+	}
+
+	public JLabel getLblCgames() {
+		return lblCgames;
+	}
+
+	public JLabel getLblCfurs() {
+		return lblCfurs;
+	}
+
+	public JLabel getLblCwater() {
+		return lblCwater;
+	}
+
+	public ButtonGroup getBuyGroup() {
+		return buyGroup;
+	}
+
+	public ButtonGroup getSellGroup() {
+		return sellGroup;
+	}
+
+	public JButton getWaterBuy() {
+		return waterBuy;
+	}
+
+	public JButton getFursBuy() {
+		return fursBuy;
+	}
+
+	public JButton getGamesBuy() {
+		return gamesBuy;
+	}
+
+	public JButton getFoodBuy() {
+		return foodBuy;
+	}
+
+	public JButton getFirearmsBuy() {
+		return firearmsBuy;
+	}
+
+	public JButton getMachinesBuy() {
+		return machinesBuy;
+	}
+
+	public JButton getMedicineBuy() {
+		return medicineBuy;
+	}
+
+	public JButton getNarcoticsBuy() {
+		return narcoticsBuy;
+	}
+
+	public JButton getOreBuy() {
+		return oreBuy;
+	}
+
+	public JButton getRobotsBuy() {
+		return robotsBuy;
+	}
+
+	public JButton getWaterSell() {
+		return waterSell;
+	}
+
+	public JButton getFursSell() {
+		return fursSell;
+	}
+
+	public JButton getGamesSell() {
+		return gamesSell;
+	}
+
+	public JButton getFoodSell() {
+		return foodSell;
+	}
+
+	public JButton getFirearmsSell() {
+		return firearmsSell;
+	}
+
+	public JButton getMachinesSell() {
+		return machinesSell;
+	}
+
+	public JButton getMedicineSell() {
+		return medicineSell;
+	}
+
+	public JButton getNarcoticsSell() {
+		return narcoticsSell;
+	}
+
+	public JButton getOreSell() {
+		return oreSell;
+	}
+
+	public JButton getRobotsSell() {
+		return robotsSell;
 	}
 	
 	public JPanel getPanel() {
